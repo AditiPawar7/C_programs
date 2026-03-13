dacba3cf97f2726ecec982b027da617a853ce090
AditiPawar7 <aditipawar6122004@gmail.com>
2026-03-13 21:13:38 +0530

    Add files via upload

---
+++ Assignment No.1/worldprint.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int num;
+
+    printf("Enter Number :");
+    scanf("%d",&num);
+
+    if(num < 10){
+        printf("Hello");
+    }
+    else{
+        printf("world");
+    }
+
+
+getch();
+return 0;
+
+}
