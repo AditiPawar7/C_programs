fe30d8c5b5638b76dccdf0e704a5474eec015b6c
AditiPawar7 <aditipawar6122004@gmail.com>
2026-03-13 21:10:31 +0530

    Add files via upload

---
+++ Assignment No.1/odd_ternaryoperator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int num ;
+
+    printf("Enter a Number :");
+    scanf("%d",&num);
+
+    (num % 2 == 0) ? printf("\nThe number is even"):  printf("\n The number is odd");
+
+
+
+ getch();
+ return 0;
+
+
+
+
+}
