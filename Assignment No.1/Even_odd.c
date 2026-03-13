73015141ed2d8c6d0d6f5730f759cd51f9b29101
AditiPawar7 <aditipawar6122004@gmail.com>
2026-03-13 21:02:36 +0530

    Add files via upload

---
+++ Assignment No.1/Even_odd.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+ int num = 0;
+
+ printf("Enter a number :");
+ scanf("%d",&num);
+
+ if(num % 2 == 0){
+
+    printf("\n%d is even number",num);
+
+  }
+  else{
+    printf("\n%d is odd number",num);
+  }
+
+  getch();
+  return 0;
+ }
+
+
+
+
+
+
+
+
+
