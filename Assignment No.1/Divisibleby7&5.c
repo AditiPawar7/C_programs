6961750f5af95bf2d782c5baf70a792c20b4b838
AditiPawar7 <aditipawar6122004@gmail.com>
2026-03-13 21:09:42 +0530

    Add files via upload

---
+++ Assignment No.1/Divisibleby7&5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int num ;
+
+    printf("Enter a Number :");
+    scanf("%d",&num);
+
+    if(num % 7 == 0 && num % 5 == 0){
+
+        printf("\n%d is divisible by 5 & 7",num);
+    }
+    else{
+         printf("\n%d is not divisible by 5 & 7",num);
+    }
+
+ getch();
+ return 0;
+
+
+
+
+}
